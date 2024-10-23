3020de0845047ae36b0ffd5713012caf88edbe8e
baptistelaldev <vbf.digi@gmail.com>
2024-10-23 00:03:50 -0300

    teste na home

--- src/app/pages/tabs/explorar/explorar.module.ts
+++ src/app/pages/tabs/explorar/explorar.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ExplorarPageRoutingModule } from './explorar-routing.module';
 
 import { ExplorarPage } from './explorar.page';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ComponentsModule } from "../../../shared/components/components.module";
 
 @NgModule({
   imports: [
     SharedModule,
-    ExplorarPageRoutingModule
-  ],
-  declarations: [ExplorarPage]
+    ExplorarPageRoutingModule,
+    ComponentsModule
+],
+  declarations: [ExplorarPage],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class ExplorarPageModule {}
