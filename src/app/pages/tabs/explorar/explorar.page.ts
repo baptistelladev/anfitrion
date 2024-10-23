8c51a909f97987d68ca04818f167c0f51fd541e0
baptistelaldev <vieirabaptistellafelipe@gmail.com>
2024-10-23 00:03:50 -0300

    teste na home

--- src/app/pages/tabs/explorar/explorar.page.ts
+++ src/app/pages/tabs/explorar/explorar.page.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { ILang } from 'src/app/shared/models/Lang';
+import * as AppStore from './../../../shared/store/app.state';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'rgs-explorar',
   templateUrl: './explorar.page.html',
   styleUrls: ['./explorar.page.scss'],
 })
-export class ExplorarPage implements OnInit {
+export class ExplorarPage implements OnInit, AfterViewInit {
 
-  constructor() { }
+  public currentLanguage: ILang;
+  public currentLanguage$: Observable<ILang>;
+  public currentLanguageSubscription: Subscription;
+
+  @ViewChild('explorarSwiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+
+  public segments: any[] = [
+    {
+      value: 'CIDADE'
+    },
+    {
+      value: 'PRAIA'
+    }
+  ]
+
+  public selectedSegment: string = 'CIDADE';
+
+  constructor(
+    private store : Store
+  ) { }
 
   ngOnInit() {
+    this.getCurrentLanguageFromNGRX();
+  }
+
+  ngAfterViewInit(): void {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  public getCurrentLanguageFromNGRX(): void {
+    this.currentLanguage$ = this.store.select(AppStore.selectAppCurrentLanguage);
+
+    this.currentLanguageSubscription = this.currentLanguage$
+    .subscribe((language: ILang) => {
+      this.currentLanguage = language;
+    })
+  }
+
+  public segmentChanged(): void {
+    console.log('dada');
   }
 
 }
