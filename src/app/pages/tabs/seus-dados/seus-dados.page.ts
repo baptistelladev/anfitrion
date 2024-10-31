94dc6e571c281d548d1dafebc0d749099d576257
baptistelaldev <vbf.digi@gmail.com>
2024-10-31 16:16:11 -0300

    criando esqueleto de telas inexistentes

--- src/app/pages/tabs/seus-dados/seus-dados.page.ts
+++ src/app/pages/tabs/seus-dados/seus-dados.page.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { ILang } from 'src/app/shared/models/ILang';
+import * as AppStore from './../../../shared/store/app.state';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { IUSer } from 'src/app/shared/models/IUser';
+import * as UserStore from './../../../shared/store/user.state';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'rgs-seus-dados',
   templateUrl: './seus-dados.page.html',
   styleUrls: ['./seus-dados.page.scss'],
 })
-export class SeusDadosPage implements OnInit {
+export class SeusDadosPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  public currentLanguage: ILang;
+  public currentLanguage$: Observable<ILang>;
+  public currentLanguageSubscription: Subscription;
+
+  public user: IUSer;
+  public user$: Observable<IUSer>;
+  public userSubscription: Subscription;
+
+  public personalDataForm: FormGroup;
+
+  constructor(
+    private navCtrl : NavController,
+    private store : Store,
+    private formBuilder : FormBuilder,
+    private title : Title
+  ) { }
 
   ngOnInit() {
+    this.getUserFromNGRX();
+    this.getCurrentLanguageFromNGRX();
+    this.initPersonalDataForm();
+  }
+
+  ionViewdidEnter(): void {
+    this.title.setTitle('Seus dados');
+  }
+
+  public back(): void {
+    this.navCtrl.back()
+  }
+
+  public getCurrentLanguageFromNGRX(): void {
+    this.currentLanguage$ = this.store.select(AppStore.selectAppCurrentLanguage);
+
+    this.currentLanguageSubscription = this.currentLanguage$
+    .subscribe((language: ILang) => {
+      this.currentLanguage = language;
+    })
+  }
+
+  public initPersonalDataForm(): void {
+    this.personalDataForm = this.formBuilder.group({
+      firstName: null,
+      email: null
+    })
+  }
+
+  public getUserFromNGRX(): void {
+    this.user$ = this.store.select(UserStore.selectUser);
+
+    this.userSubscription = this.user$
+    .subscribe((user: IUSer) => {
+      this.user = user;
+      console.log(this.user);
+    })
+  }
+
+  public ngOnDestroy(): void {
+    this.currentLanguageSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
 }
--- src/app/pages/tabs/sua-conta/sua-conta.module.ts
+++ src/app/pages/tabs/sua-conta/sua-conta.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { SuaContaPageRoutingModule } from './sua-conta-routing.module';
 
 import { SuaContaPage } from './sua-conta.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SuaContaPageRoutingModule
+    SharedModule,
+    SuaContaPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [SuaContaPage]
 })
