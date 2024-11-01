16231217486c156765cd1278213bf58533f2fe84
baptistelaldev <vbf.digi@gmail.com>
2024-11-01 20:40:20 -0300

    ajustes na tela de dados, integrando form

--- src/app/pages/tabs/seus-dados/seus-dados.page.ts
+++ src/app/pages/tabs/seus-dados/seus-dados.page.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { ILang } from 'src/app/shared/models/ILang';
 import * as AppStore from './../../../shared/store/app.state';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IUSer } from 'src/app/shared/models/IUser';
 import * as UserStore from './../../../shared/store/user.state';
 import { Title } from '@angular/platform-browser';
@@ -15,6 +15,8 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./seus-dados.page.scss'],
 })
 export class SeusDadosPage implements OnInit, OnDestroy {
+
+  public showBirthDateModal: boolean = false;
 
   public currentLanguage: ILang;
   public currentLanguage$: Observable<ILang>;
@@ -34,9 +36,9 @@ export class SeusDadosPage implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
+    this.initPersonalDataForm();
     this.getUserFromNGRX();
     this.getCurrentLanguageFromNGRX();
-    this.initPersonalDataForm();
   }
 
   ionViewdidEnter(): void {
@@ -58,8 +60,10 @@ export class SeusDadosPage implements OnInit, OnDestroy {
 
   public initPersonalDataForm(): void {
     this.personalDataForm = this.formBuilder.group({
-      firstName: null,
-      email: null
+      name: [ '', [ Validators.required, Validators.minLength(3) ] ],
+      birthDateAsDate: null,
+      birthDateAsText: null,
+      secondName: null
     })
   }
 
@@ -70,7 +74,32 @@ export class SeusDadosPage implements OnInit, OnDestroy {
     .subscribe((user: IUSer) => {
       this.user = user;
       console.log(this.user);
+
+      this.personalDataForm.patchValue({
+        name: this.user.firstName
+      })
     })
+  }
+
+  public clearFieldKeepJustName(event: any): void {
+    let name: string = this.personalDataForm.value.name.replace(/[^a-zA-ZÀ-ÿ]/g, '');
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    this.personalDataForm.patchValue({ name: name });
+  }
+
+  public preventWhitespace(event: KeyboardEvent) {
+    if (event.key === ' ') {
+      event.preventDefault();
+    }
+  }
+
+  public toggleBirthDateModal(show: boolean): void {
+    this.showBirthDateModal = show;
+  }
+
+  public birthDateChanged(e: any): void {
+    console.log(e);
+
   }
 
   public ngOnDestroy(): void {
