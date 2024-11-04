3e0d9c1d343a4b110625931ab910e8bbc796a70e
baptistelaldev <vbf.digi@gmail.com>
2024-11-04 00:03:27 -0300

    criando modais da tela de conta

--- src/app/pages/tabs/sua-conta/sua-conta.page.ts
+++ src/app/pages/tabs/sua-conta/sua-conta.page.ts
@@ -4,10 +4,11 @@ import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { ILang } from 'src/app/shared/models/ILang';
 import * as AppStore from './../../../shared/store/app.state';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IUSer } from 'src/app/shared/models/IUser';
 import * as UserStore from './../../../shared/store/user.state';
 import { Title } from '@angular/platform-browser';
+import { UtilsService } from 'src/app/core/services/utils.service';
 
 @Component({
   selector: 'anfitrion-sua-conta',
@@ -15,6 +16,16 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./sua-conta.page.scss'],
 })
 export class SuaContaPage implements OnInit, OnDestroy {
+
+  public passwordIsValid: boolean = false;
+  public passwordRules: any[];
+  public passwordsMatch: boolean = false;
+
+  public showPassword: boolean = false;
+  public showConfirmPassword: boolean = false;
+
+  public showNewEmailModal: boolean = false;
+  public showNewPasswordModal: boolean = false;
 
   public emailFieldIsDisabled: boolean = true;
 
@@ -28,22 +39,43 @@ export class SuaContaPage implements OnInit, OnDestroy {
 
   public accountForm: FormGroup;
 
+  public newEmailFormGroup: FormGroup;
+  public newPasswordFormGroup: FormGroup;
+
   constructor(
     private navCtrl : NavController,
     private store : Store,
     private formBuilder : FormBuilder,
-    private title : Title
+    private title : Title,
+    private utilsService : UtilsService
   ) { }
 
   ngOnInit() {
+    this.getPasswordRules();
     this.getUserFromNGRX();
     this.getCurrentLanguageFromNGRX();
     this.initAccountForm();
     this.fillAccountForm();
+
+    this.initNewEmailForm();
+    this.initNewPasswordForm();
   }
 
   ionViewdidEnter(): void {
     this.title.setTitle('Sua conta');
+  }
+
+  public initNewEmailForm(): void {
+    this.newEmailFormGroup = this.formBuilder.group({
+      newEmail: [null, [ Validators.required ]]
+    })
+  }
+
+  public initNewPasswordForm(): void {
+    this.newPasswordFormGroup = this.formBuilder.group({
+      password: [ '', [ Validators.required, Validators.minLength(8) ] ],
+      confirmPassword: [ '', [ Validators.required, Validators.minLength(8) ] ]
+    })
   }
 
   public back(): void {
@@ -86,6 +118,42 @@ export class SuaContaPage implements OnInit, OnDestroy {
     if (event.key === ' ') {
       event.preventDefault();
     }
+  }
+
+  public toggleNewEmailModal(show: boolean): void {
+    this.showNewEmailModal = show;
+  }
+
+  public toggleNewPasswordModal(show: boolean): void {
+    this.showNewPasswordModal = show;
+  }
+
+  public togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  public toggleConfirmPassword(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  public checkPasswordRules(): void {
+    let senha: string = this.newPasswordFormGroup.get('password')?.value;
+    this.utilsService.checkPasswordRules(senha);
+    this.passwordIsValid = this.passwordRules.every((rule) => rule.valid === true);
+
+    this.checkPasswordsMatch();
+  }
+
+  public checkPasswordsMatch(): boolean {
+    this.passwordsMatch = this.newPasswordFormGroup.value.password === this.newPasswordFormGroup.value.confirmPassword
+    return this.passwordsMatch
+  }
+
+  /**
+   * @description Obtém as regras de senha.
+   */
+  public getPasswordRules(): void {
+    this.passwordRules = this.utilsService.getPasswordRules();
   }
 
   public ngOnDestroy(): void {
