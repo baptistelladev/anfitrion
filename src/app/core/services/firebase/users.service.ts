e407fe918d02513c33edf7081b6731c129bb74cb
baptistelaldev <vieirabaptistellafelipe@gmail.com>
2024-11-11 16:38:21 -0300

    melhorando troca de email

--- src/app/core/services/firebase/users.service.ts
+++ src/app/core/services/firebase/users.service.ts
@@ -3,6 +3,9 @@ import { doc, Firestore, getDoc } from '@angular/fire/firestore';
 import { Store } from '@ngrx/store';
 import { IUSer } from 'src/app/shared/models/IUser';
 import * as UserStore from './../../../shared/store/user.state';
+import { Auth, EmailAuthProvider, sendEmailVerification, updateEmail, verifyBeforeUpdateEmail } from '@angular/fire/auth';
+import { reauthenticateWithCredential, UserCredential } from 'firebase/auth';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,8 @@ export class UsersService {
 
   constructor(
     private firestore : Firestore,
-    private store : Store
+    private store : Store,
+    private auth: Auth
   ) { }
 
   public async getUserByUID(collectionName: string, docId: string): Promise<boolean> {
@@ -37,5 +41,29 @@ export class UsersService {
       this.store.dispatch(UserStore.setUser({ user }));
       resolve();
     });
+  }
+
+  public async updateUserEmail(newEmail: string, currentPassword: string): Promise<boolean> {
+    const user = this.auth.currentUser;
+
+    if (!user) {
+      throw new Error('Usuário não autenticado');
+    }
+
+    const credential = EmailAuthProvider.credential(user.email || '', currentPassword);
+
+    try {
+      // Reautentica o usuário
+      await reauthenticateWithCredential(user, credential);
+
+      // Envia o email de verificação para o novo endereço
+      await verifyBeforeUpdateEmail(user, newEmail);
+
+      console.log('Email de verificação enviado para o novo email!');
+      return true;
+    } catch (error) {
+      console.error('Erro ao atualizar o e-mail:', error);
+      throw error; // Lança o erro para ser tratado pelo chamador
+    }
   }
 }
