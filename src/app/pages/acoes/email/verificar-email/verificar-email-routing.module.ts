e98a95848d8fe39bff27e7723e70c68565016531
baptistelaldev <vieirabaptistellafelipe@gmail.com>
2024-11-12 12:02:34 -0300

    refatoração confirmação de email

--- src/app/pages/acoes/email/verificar-email/verificar-email-routing.module.ts
+++ src/app/pages/acoes/email/verificar-email/verificar-email-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { VerificarAlteracaoDeEmailPage } from './verificar-alteracao-de-email.page';
+import { VerificarEmailPage } from './verificar-email.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: VerificarAlteracaoDeEmailPage
+    component: VerificarEmailPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class VerificarAlteracaoDeEmailPageRoutingModule {}
+export class VerificarEmailPageRoutingModule {}
