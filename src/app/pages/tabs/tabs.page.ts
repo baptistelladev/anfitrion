ee85036801947378f8e950e55c44846d8b85b91d
baptistelaldev <vbf.digi@gmail.com>
2024-11-28 23:30:52 -0300

    melhorias visuais nas tabs

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,7 +1,9 @@
 import { MOCK_TABS } from 'src/app/shared/mocks/MockTabs';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ITab } from 'src/app/shared/models/ITab';
+import { NavController } from '@ionic/angular';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'anfitrion-tabs',
@@ -14,23 +16,32 @@ export class TabsPage implements OnInit {
   public applyDarkTabBar: boolean = false;
   public hideTabs: boolean = false;
   public currentUrlJustString: string;
+  public currentPage: string;
 
   constructor(
-    private router : Router
+    private router : Router,
+    private navCtrl : NavController
   ) { }
 
   ngOnInit() {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      let url = event.urlAfterRedirects;
+
+      if (url === '/logado/sobre-nos') { this.applyDarkTabBar = true } else { this.applyDarkTabBar = false }
+      if (url === '/logado/bem-vindo-a-baixada-santista') { this.hideTabs = true } else { setTimeout(() => {
+        this.hideTabs = false
+      }, 100); }
+
+      this.currentUrlJustString = this.router.url.split('/')[2];
+      this.currentPage = this.currentUrlJustString;
+    });
   }
 
-  public checkRoute(value: any) {
-    if (this.router.url === '/logado/sobre-nos') { this.applyDarkTabBar = true } else { this.applyDarkTabBar = false }
-    if (this.router.url === '/logado/bem-vindo-a-baixada-santista') { this.hideTabs = true } else { setTimeout(() => {
-      this.hideTabs = false
-    }, 100); }
-
-
-    this.currentUrlJustString = this.router.url.split('/')[1];
-
+  public segmentHasChanged(ev: any): void {
+    this.currentPage = ev.detail.value;
+    this.navCtrl.navigateRoot(['/logado/' + this.currentPage]);
   }
 
 }
