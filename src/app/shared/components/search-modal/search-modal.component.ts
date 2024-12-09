a599b93a00dc245eec3adeb04194f7ef3ed81448
baptistelaldev <vieirabaptistellafelipe@gmail.com>
2024-12-08 21:03:01 -0300

    testando indicacao de japa

--- src/app/shared/components/search-modal/search-modal.component.ts
+++ src/app/shared/components/search-modal/search-modal.component.ts
@@ -1,15 +1,18 @@
 import { MOCK_CITY_FEATURES } from 'src/app/shared/mocks/MockCityFeatures';
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { IonInput, ModalController } from '@ionic/angular';
 import { ICity } from '../../models/ICity';
 import { ILang } from '../../models/ILang';
+import { PlacesService } from 'src/app/core/services/firebase/places.service';
+import { CollectionsEnum } from '../../enums/Collection';
+import { IPlace } from '../../models/IPlace';
 
 @Component({
   selector: 'anfitrion-search-modal',
   templateUrl: './search-modal.component.html',
   styleUrls: ['./search-modal.component.scss'],
 })
-export class SearchModalComponent  implements OnInit {
+export class SearchModalComponent  implements OnInit, AfterViewInit {
 
   @Input() currentLang: ILang;
   @Input() currentCityAsParam: ICity | undefined;
@@ -17,15 +20,50 @@ export class SearchModalComponent  implements OnInit {
   @Input() id: string;
   @Input() placeTypeOBJ: any;
 
+  @ViewChild('searchbar', { static: true }) searchbar: IonInput;
+
+  public places: IPlace[];
+
   constructor(
-    private modalCtrl : ModalController
+    private modalCtrl : ModalController,
+    private placesService : PlacesService
   ) { }
 
   ngOnInit() {
+
+  }
+
+  async ngAfterViewInit() {
+    setTimeout(async () => {
+      await this.focusOnInput();
+    }, 500);
   }
 
   public async closeModal() {
     await this.modalCtrl.dismiss('', '', this.id);
   }
 
+  public async focusOnInput() {
+    this.searchbar.setFocus();
+  }
+
+  public isTyping(e: any) {
+    let textTransformed: string = this.transformString(e.detail.value);
+
+    this.placesService.getCollection(CollectionsEnum.PLACES, [
+      { field: 'value', operator: '==', value: textTransformed }
+    ]).subscribe({
+      next: (places: IPlace[]) => {
+        this.places = places;
+      }
+    })
+  }
+
+  public transformString(text: string): string {
+    if (!text) return '';
+    let transformed = text.toLowerCase();
+    transformed = transformed.trim();
+    transformed = transformed.replace(/\s+/g, '-');
+    return transformed
+  }
 }
